deb1fd0b200712c3c3aecd0688f46288f34fb16c
Jithin K Sunil <jithinksunil96@gmail.com>
2026-02-14 17:04:57 +0530

    updates

--- requests/fetchGifs.requests.ts
+++ requests/fetchGifs.requests.ts
@@ -4,7 +4,8 @@ import { GiphyFetch } from '@giphy/js-fetch-api';
 export const fetchGifs = (search: Search) => {
   const gf = new GiphyFetch(process.env.NEXT_PUBLIC_GIFY_API_KEY!);
   return gf.search(search.query, {
-    limit: 10,
+    limit: 100,
     rating: 'g',
+    offset: (search.page - 1) * 10,
   });
 };
