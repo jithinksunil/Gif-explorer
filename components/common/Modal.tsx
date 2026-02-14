2377d40c70d49f88ea5f2b1790ab440b48f6c0f2
Jithin K Sunil <jithinksunil96@gmail.com>
2026-02-14 16:27:55 +0530

    updates

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -14,8 +14,7 @@ export function Modal({ imageUrl, children }: ModalProps) {
       <Dialog.Trigger asChild>{children}</Dialog.Trigger>
 
       <Dialog.Portal>
-        <Dialog.Overlay className='fixed inset-0 bg-black/50 z-50' />
-
+        <Dialog.Overlay className='fixed inset-0 bg-black/70 z-50' />
         <Dialog.Content className='fixed left-1/2 top-1/2 z-50 max-h-[80vh]  -translate-x-1/2 -translate-y-1/2 bg-background p-6 rounded-3xl overflow-hidden outline-none focus:outline-none'>
           <img src={imageUrl} alt={imageUrl} className='rounded-[16px]'/>
         </Dialog.Content>
