3a39bc3cd7719e1e7e6a0686afb2001a14ef69b9
jithinksunil <jithinksunil96@gmail.com>
2026-02-15 16:37:44 +0530

    tweaks

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -17,7 +17,12 @@ export function Modal({ gif, children }: ModalProps) {
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 bg-black/70 z-50' />
         <Dialog.Content className='fixed left-1/2 top-1/2 z-50 max-h-[80vh]  -translate-x-1/2 -translate-y-1/2 bg-background p-6 rounded-3xl overflow-hidden outline-none focus:outline-none'>
-          <img src={gif.originalUrl} alt={gif.title} className='rounded-[16px] h-full w-auto' />
+          <img
+            src={gif.originalUrl}
+            alt={gif.title}
+            className='rounded-[16px] h-full'
+            style={{ aspectRatio: gif.originalWidth / gif.originalHeight }}
+          />
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
