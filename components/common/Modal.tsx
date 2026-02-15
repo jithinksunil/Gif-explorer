3270f5e83ec6680a5b55770c4842d6969a1bd8f3
jithinksunil <jithinksunil96@gmail.com>
2026-02-15 16:38:29 +0530

    updates

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -16,7 +16,7 @@ export function Modal({ gif, children }: ModalProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 bg-black/70 z-50' />
-        <Dialog.Content className='fixed left-1/2 top-1/2 z-50 max-h-[80vh]  -translate-x-1/2 -translate-y-1/2 bg-background p-6 rounded-3xl overflow-hidden outline-none focus:outline-none'>
+        <Dialog.Content className='fixed left-1/2 top-1/2 z-50 h-[80vh]  -translate-x-1/2 -translate-y-1/2 bg-background p-6 rounded-3xl overflow-hidden outline-none focus:outline-none'>
           <img
             src={gif.originalUrl}
             alt={gif.title}
